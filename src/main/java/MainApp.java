901a415f4dbf7540aae8aedeceb080b6e9d6aa2f
alinaberlin <alina.ghetler@gmail.com>
2023-10-03 21:41:24 +0200

    Add Manifest, Jar files

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class MainApp {
+public class MainApp {
+    public static void main(String[] args){
+        StudentManager studentManager = new StudentManager();
+        studentManager.addStudent(new Student("Gugu", 10, 8));
+        studentManager.addStudent(new Student("Gugi", 11, 8));
+        studentManager.displayStudents();
+    }
 }
--- src/main/java/StudentManager.java
+++ src/main/java/StudentManager.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class StudentManager {
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentManager {
+    List<Student> students=new ArrayList<>();
+    public  void addStudent(Student student){
+        students.add(student);
+    }
+    public  void removeStudent(Student student){
+        students.remove(student);
+    }
+    public  void displayStudents(){
+        for(int i = 0; i<students.size(); i++ ) {
+            System.out.println(students.get(i).getName());
+        }
+    }
 }

3ee7f66e1c19180990f25749544c890d0b03e58c
alinaberlin <alina.ghetler@gmail.com>
2023-10-05 11:03:19 +0200

    Add search method and searchable interface

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,8 +1,12 @@
+import java.awt.*;
+
 public class MainApp {
     public static void main(String[] args){
         StudentManager studentManager = new StudentManager();
         studentManager.addStudent(new Student("Gugu", 10, 8));
         studentManager.addStudent(new Student("Gugi", 11, 8));
         studentManager.displayStudents();
+        Student student= studentManager.search("gugu");
+        System.out.println("My student is "+ student.getName());
     }
 }
--- src/main/java/StudentManager.java
+++ src/main/java/StudentManager.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class StudentManager {
+public class StudentManager implements Searcheable{
     List<Student> students=new ArrayList<>();
     public  void addStudent(Student student){
         students.add(student);
@@ -13,5 +13,18 @@ public class StudentManager {
         for(int i = 0; i<students.size(); i++ ) {
             System.out.println(students.get(i).getName());
         }
+    }
+
+    @Override
+    public Student search(String name) {
+        for (int i=0; i<students.size(); i++){
+            Student student = students.get(i);
+            if (student.getName().toLowerCase().equals(name.toLowerCase()) ){
+                return student;
+            }
+        }
+
+
+        return null;
     }
 }
